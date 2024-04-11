d1c9a937a7638353a80a9bb02129ce00296ae233
Balaramakrishna <balaramakrishna.p.varma@gmail.com>
2024-04-11 10:15:56 +0530

    Implemented logging, required to check other functionality

--- bootstrap.py
+++ bootstrap.py
@@ -1,6 +1,12 @@
 ## Stage 1
+from kafka import KafkaProducer, KafkaConsumer
+import json
 
 
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+log = { 'name': 'John', 'age': 30 }
+producer.send("logs", json.dumps(log).encode('utf-8'))
+producer.flush()
 
 
 
--- logger.py
+++ logger.py
@@ -3,11 +3,12 @@ import json
 
     
 if __name__ == '__main__':
-    consumer = KafkaConsumer('logs', bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
-    log_file = open('logs.txt', 'w')
+    consumer = KafkaConsumer('logs', bootstrap_servers='localhost:9092')  ## earliest to get all the logs
     for msg in consumer:
         json_string = msg.value.decode('utf-8')
         log = json.loads(json_string)
         
+        log_file = open('logs.txt', 'a')
         log_file.write(json.dumps(log, indent=4) + '\n')
+        log_file.close()
         print(json.dumps(log, indent=4) + '\n')
