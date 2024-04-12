f01ccc63eeed376f062bdc4cf121df164d3fed51
Balaramakrishna <balaramakrishna.p.varma@gmail.com>
2024-04-12 07:49:22 +0530

    Directory structure change

--- exp.py
+++
@@ -1,3 +0,0 @@
-from kafka import KafkaProducer, KafkaConsumer
-consumer = KafkaConsumer('logs', bootstrap_servers='localhost:9092', auto_offset_reset='latest')
-msg = next(consumer)
--- vm_manager.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaProducer, KafkaConsumer
-import json
-
-
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-log = { 'Process': 'vm_manager', 'message': 'I have been run' }
-producer.send("logs", json.dumps(log).encode('utf-8'))
-producer.flush()
-
